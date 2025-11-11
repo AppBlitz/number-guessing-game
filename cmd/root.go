023d1f0961976486eee8c35b59d32f6d1b84fde9
Carlos Fabian Corrales Zapata <fabianzapata98@outlook.com>
2025-11-11 10:54:50 -0500

    directories

---
+++ cmd/root.go
@@ -0,0 +1,18 @@
+// Package cmd in create command for line
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
